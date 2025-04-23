634600f87557a4e0949ceffccc59cb7968507042
VanhPoker <vietanh951325@gmail.com>
2025-04-23 14:52:04 +0700

    Sua component header

--- component/Layout/Header/index.tsx
+++ component/Layout/Header/index.tsx
@@ -1,15 +1,53 @@
+import { AlignJustify, Bell, ChevronDown, Search } from "lucide-react"
 import Image from "next/image"
 import React from "react"
 export default function Header(){
-return (   <div className="flex border-b-1 border-gray-300 min-h-[60px]">
-    <div className="w-1/3 flex ">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-align-justify-icon lucide-align-justify"><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg>
-    <Image src="/images/gkebook-logo.png" width={50} height={100} alt=" gke book" />
+return ( 
+      <div className="flex border-b-1 justify-between items-center border-gray-300 min-h-[72px]">
+    <div className=" flex gap-1 ">
+    <AlignJustify size={18} strokeWidth={1.75} className="ml-5 mt-2 text-gray-700" />
+    <Image src="/images/gkebook-logo.png" width={70} height={120} alt=" gke book" className="ml-5" />
 
     </div>
-    <div className="w-1/3 ">
-    <input type="text" />
+    <div className="w-1/3">
+    <div className="flex items-center bg-white border border-gray-300 rounded-lg p-2 shadow-sm w-full py-3 ml-20 ">
+      <Search size={20} className="text-gray-500 mr-2" />
+      <input
+        type="text"
+        placeholder="Tìm kiếm sách bài tập ,học liệu...."
+        className="bg-transparent border-none outline-none text-sm text-gray-700 placeholder-gray-400 w-full"
+      />
     </div>
-    <div className="w-1/3 ">1</div>
-    </div>)
+    </div>
+    <div className="mr-10">
+    <div className="flex items-center gap-3">
+     
+      <button className="bg-white text-gray-800 border-gray-300 shadow-sm px-4 py-2 rounded-md text-sm border-1">
+        Luyện tập
+      </button>
+
+
+      <div className="flex items-center gap-1 bg-blue-900 text-white px-3 py-2 rounded-md my-2 mr-6">
+        <span className="text-sm">Tạo</span>
+        <ChevronDown size={16} />
+      </div>
+
+     
+      <div className="text-xl cursor-pointer ">
+        <Bell size={18} />
+      </div>
+
+     
+      <div>
+        <img
+          src="/images/robot.png"
+          alt="User Avatar"
+          className="w-9 h-9 rounded-full bg-gradient-to-r from-gray-800 ml-2 to-gray-300 cursor-pointer"
+        />
+      </div>
+    </div>
+    </div>
+    </div>
+    
+)
 }
