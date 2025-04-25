01f6b6726ba906a8742b5554385bf69eb0a715b7
VanhPoker <vietanh951325@gmail.com>
2025-04-25 09:43:54 +0700

    ok

--- component/Layout/Filter/index.tsx
+++ component/Layout/Filter/index.tsx
@@ -3,8 +3,32 @@ export default function Filter(){
 return (   
 
     <div className="flex w-full ">
-        <div className="w-1/3 bg-amber-200">1</div>
-        <div className="w-2/3 bg-amber-700">1</div>
+        <div className="flex flex-col">
+            <span>Kho học liệu</span>
+            <span>Tổng hợp học liệu hiện có trên hệ thống</span>
+        </div>
+        <div className="flex flex-col">
+           
+            <label htmlFor="level">Khối lớp</label>
+<select name="level" id="level">
+  <option value="Khoi 1">Khối 1 </option>
+  <option value="Khoi 2">Khối 2</option>
+  <option value="Khoi 3">Khối 3</option>
+  <option value="Khoi 4">Khối 4</option>
+  <option value="Khoi 5">Khối 5</option>
+  <option value="Khoi 6">Khối 6</option>
+  <option value="Khoi 7">Khối 7</option>
+  <option value="Khoi 8">Khối 8</option>
+  <option value="Khoi 9">Khối 9</option>
+  <option value="Khoi 10">Khối 10</option>
+  <option value="Khoi 11">Khối 11</option>
+  <option value="Khoi 12">Khối 12</option>
+</select>
+
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 )
 }
--- component/Layout/Menu/index.tsx
+++ component/Layout/Menu/index.tsx
@@ -1,17 +1,38 @@
+import { Box, File, FileKey, Headphones, ListTodo, Play, Presentation, Puzzle } from "lucide-react";
 import React from "react";
 export default function Menu(){
 return (   
 
-    <div className="flex bg-amber-300 h-20">
-       <div></div>
-       <div></div>
-       <div></div>
-       <div></div>
-       <div></div>
-       <div></div>
-       <div></div>
-       <div></div>
-       <div></div>
+    <div className="">
+      <div className="flex  items-center justify-between ">
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500" tabIndex={0}>
+             <p>Tất cả</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+             <ListTodo size={20}  strokeWidth={1.75} /><p>Bộ đề</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+          <Presentation /> <p>Bài tập </p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500" tabIndex={0}>
+          <FileKey /><p>Scorm/xAPI</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+          <File /> <p>Tài liệu</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+          <Headphones /> <p>Âm thanh</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+          <Play /> <p>Video</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+          <Box /> <p>3D/VR</p>
+          </div>
+          <div className="flex gap-2 pl-3 border-b-3 basis-1/3 border-white hover:border-b-3 hover:border-orange-500 py-2 focus:border-b-3 focus:border-orange-500 text-gray-500 hover:text-orange-500"tabIndex={0}>
+          <Puzzle /> <p>Quizz</p>
+          </div>
+        </div>
     </div>
 )
 }
